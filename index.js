cef9fbe2dc9ffe911d609fc1bfd2f11b2efb616d
Maruslam <lan29138@gmail.com>
2025-09-13 20:46:50 +0700

    update cors

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
-// import express from "express";
-// import { google } from "googleapis";
-
-// const app = express();
-// app.use(express.json());
-
-// // โหลด credential (service account JSON)
-// const auth = new google.auth.GoogleAuth({
-//   keyFile: "credentials.json",
-//   scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-// });
-// const sheets = google.sheets({ version: "v4", auth });
-
 import express from "express";
 import { google } from "googleapis";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // --- FIXED CREDENTIALS ---
 const credentials = {
